39c4f162b5d871a31f935e4efcd2967147c0be6e
Anjana George <anjana7581@gmail.com>
2024-12-30 14:41:49 +0530

    footer responsive

--- fabrivo-frontend/src/components/Footer/Footer.jsx
+++ fabrivo-frontend/src/components/Footer/Footer.jsx
@@ -1,35 +1,51 @@
-import './Footer.css'
+import './Footer.css';
 
-function Footer(){
-    return(
+function Footer() {
+    return (
         <div className="footer-main">
-            
-          <div className="footer-one">
-             <h1>FabRivo</h1>
-             <p>Social Media</p>
-
-          </div>
-          <div className="footer-two">
-             <h4>Shop</h4>
-             <li>Products</li>
-             <li>Overview</li>
-             <li>Prizing</li>
-             <li>Releases</li>
-             
-          </div>
-          <div className="footer-three">
-             <h4>Company</h4>
-               <li>About Us</li>
-               <li>Contact</li>
-               <li>News</li>
-               <li>Support</li>
-          </div>
-          <div className="footer-four">
-         <h4>Stay Up To Date</h4>
-         <button type="submit" >Enter Your Email</button>
-          </div>
+            <div className="footer-section">
+                <div className="footer-one">
+                    <h1>FabRivo</h1>
+                    <p>Social Media</p>
+                    <div className="social-icons">
+                        <i className="fab fa-twitter"></i>
+                        <i className="fab fa-facebook"></i>
+                        <i className="fab fa-instagram"></i>
+                    </div>
+                </div>
+                <div className="footer-two">
+                    <h4>SHOP</h4>
+                    <ul>
+                        <li>Products</li>
+                        <li>Overview</li>
+                        <li>Pricing</li>
+                        <li>Releases</li>
+                    </ul>
+                </div>
+                <div className="footer-three">
+                    <h4>COMPANY</h4>
+                    <ul>
+                        <li>About Us</li>
+                        <li>Contact</li>
+                        <li>News</li>
+                        <li>Support</li>
+                    </ul>
+                </div>
+                <div className="footer-four">
+                    <h4>STAY UP TO DATE</h4>
+                    <div className="email-subscribe">
+                        <input type="email" placeholder="Enter your email" />
+                        <button type="submit">SUBMIT</button>
+                    </div>
+                </div>
+            </div>
+            <div className="footer-bottom">
+                <span>Terms</span>
+                <span>Privacy</span>
+                <span>Cookies</span>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Footer
+export default Footer;
